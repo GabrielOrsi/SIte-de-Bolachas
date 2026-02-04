77b3e67563ec0369940652a42a85330ce0dc3d40
GabrielOrsi <gbrlorsi39@gmail.com>
2026-02-04 14:53:45 -0300

    .

--- script.js
+++ script.js
@@ -11,3 +11,27 @@
   responsivo.addEventListener('click', abrirFecharMenu);
   closeBtn.addEventListener('click', abrirFecharMenu);
   overlay.addEventListener('click', abrirFecharMenu);
+
+
+const slides = document.querySelector('.slides');
+const slide = document.querySelectorAll('.slide');
+
+const nextBtn = document.querySelector('.btn-next');
+const prevBtn = document.querySelector('.btn-prev');
+
+let index = 0;
+
+// botão próximo
+ if ( type === 'next'){
+        btn-next.appendChild(listItem[0])
+        container.classList.add('next')
+    } else {
+        list.prepend(listItem[listItem.length -1])
+        thumb.prepend(thumbItem[thumbItem.length -1])
+        container.classList.add('back')
+    }
+
+
+function updateSlide(){
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
